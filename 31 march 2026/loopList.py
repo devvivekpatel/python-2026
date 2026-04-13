e8d9451442859d682cb5a3015a6f6057c4975551
devvivekpatel <developerpatel.vivek@gmail.com>
2026-04-13 17:39:22 +0530

    new

---
+++ 31 march 2026/loopList.py
@@ -0,0 +1,14 @@
+print('hello world')
+
+firstList = ['gagan','ram',34,100,'shyam']
+
+print(firstList)
+
+if 'gagan' in firstList:
+    print("Yes, gagan is present")
+
+for x in firstList:
+    print(x)
+
+for i in firstList:
+    print(firstList[i])
